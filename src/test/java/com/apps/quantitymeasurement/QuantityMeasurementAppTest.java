effa0cecf710feacb27e1b107410f21b34397714
Aayush Rathour <aayushrathour34@gmail.com>
2026-02-23 14:53:23 +0530

    [Aayush Rathour] Created Program for UC2: Feet and Inches measurement equality

--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -42,4 +42,43 @@ public class QuantityMeasurementAppTest {
 
         assertTrue(f1.equals(f1), "Object must be equal to itself");
     }
+    
+    
+    @Test
+    void testInchesEquality_SameValue() {
+        QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches i2 = new QuantityMeasurementApp.Inches(1.0);
+
+        assertTrue(i1.equals(i2), "1.0 inch should be equal to 1.0 inch");
+    }
+
+    @Test
+    void testInchesEquality_DifferentValue() {
+        QuantityMeasurementApp.Inches f1 = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches f2 = new QuantityMeasurementApp.Inches(2.0);
+
+        assertFalse(f1.equals(f2), "1.0 ft should not be equal to 2.0 ft");
+    }
+
+    @Test
+    void testInchesEquality_NullComparison() {
+        QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(1.0);
+
+        assertFalse(i1.equals(null), "Comparison with null should return false");
+    }
+
+    @Test
+    void testInchesEquality_NonNumericInput() {
+        QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(1.0);
+
+        assertFalse(i1.equals("1.0"), "Feet should not be equal to non-numeric object");
+    }
+    @Test 
+    void testIncheEquality_SameRefrence() {
+    	QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(1.0);
+    	assertTrue(i1.equals(i1),"object must be equal to itself");
+    	
+    }
+
+   
 }
