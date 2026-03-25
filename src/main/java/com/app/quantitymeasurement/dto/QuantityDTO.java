8172b68c63eb2abc310740139fc4053933c6f9ee
Aayush Rathour <aayushrathour34@gmail.com>
2026-03-25 12:50:50 +0530

    [Aayush Rathour]:Created fixed the testcases of Spring Backend for Quantity Measurement

--- src/main/java/com/app/quantitymeasurement/dto/QuantityDTO.java
+++ src/main/java/com/app/quantitymeasurement/dto/QuantityDTO.java
@@ -1,4 +1,4 @@
-package com.app.quantitymeasurement.model;
+package com.app.quantitymeasurement.dto;
 
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotEmpty;
